19e4f8b283989ac2360f0a4ada46a34182a693c4
maximilianschroder <142219966+maximilianschroder@users.noreply.github.com>
2024-05-16 19:42:04 +0200

    Update API.py

--- Backend/API.py
+++ Backend/API.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import json
-from typing import List, Optional
+from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -19,12 +19,31 @@ class FeatureImportance(BaseModel):
     col_name: str
     percentage_importance: float
 
-@app.get("/data/", response_model=List[FeatureImportance])
-def get_data():
+class CounterfactualExplanation(BaseModel):
+    sales_actual: float
+    sales_counterfactual: float
+    changes: List[str]
+
+@app.get("/feature_importance/", response_model=List[FeatureImportance], tags=["feature_importance"])
+def get_feature_importance():
     try:
         with open("shap_feature_importance.json", "r") as file:
             data = json.load(file)
             return data
+    except FileNotFoundError:
+        return {"error": "File not found"}
+    except Exception as e:
+        return {"error": f"An error occurred: {str(e)}"}
+
+@app.get("/counterfactual_explanations/", response_model=List[CounterfactualExplanation], tags=["counterfactual_explanations"])
+def get_counterfactual_explanations():
+    try:
+        with open("counterfactual_explanations.json", "r") as file:
+            data = json.load(file)
+            counterfactual_explanations = [
+                CounterfactualExplanation(**data)
+            ]
+            return counterfactual_explanations
     except FileNotFoundError:
         return {"error": "File not found"}
     except Exception as e:
