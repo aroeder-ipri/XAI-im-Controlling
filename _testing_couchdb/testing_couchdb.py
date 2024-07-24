b2cce466fba5375274f15709a9100735b7a33808
larsmoe <lars.moestue@uni-ulm.de>
2024-07-24 22:34:19 +0200

    testing the database

---
+++ _testing_couchdb/testing_couchdb.py
@@ -0,0 +1,19 @@
+import couchdb
+
+def main():
+    # Connect to CouchDB
+    # Update the URL to match your CouchDB host and provide the correct credentials
+    couch = couchdb.Server('http://admin:password@couchdb:5984/')
+
+    # Select the database
+    db = couch['click_events']
+
+    # Fetch all documents in the database
+    for doc_id in db:
+        doc = db[doc_id]
+        print("Document ID:", doc_id)
+        print("Data:", doc)
+
+
+if __name__ == "__main__":
+    main()
