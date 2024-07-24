296345e82bd1063e466b6363ea958fdcbd0eb76b
larsmoe <lars.moestue@uni-ulm.de>
2024-07-24 14:49:32 +0200

    changed the index files such that now unique ids are created

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,17 +1,20 @@
-/* globals Chart:false */
-
-(() => {
-    'use strict';
+async function id_api_call(){
     var apiUrl = 'https://controlling.xaidemo.de/api/id';
-    fetch(apiUrl).then(response => {
-      return response.json();
-    }).then(data => {
-      // Work with JSON data here
-      console.log(data);
-    }).catch(err => {
-      console.log('Error', err)
+    const address = await fetch(apiUrl)
+    .then((response) => response.json())
+    .then((user) => {
+        return user.id;
     });
-    function link_with_id(){
-        console.log('test')
-    }
-})();
+    return address
+}
+function link_with_id(id){
+    window.location.href = "dashboard.html?id=" + id;
+}
+function btn_click(){
+    let id = id_api_call().then(id => {
+        console.log(id);
+        link_with_id(id)
+    });
+
+};
+
