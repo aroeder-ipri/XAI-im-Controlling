32fdb5943e26a286e532a18a79fe28f6a0c8d286
maximilianschroder <142219966+maximilianschroder@users.noreply.github.com>
2024-08-15 20:44:55 +0200

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,3 +1,42 @@
+// Funktion zur zufälligen Zuweisung der Gruppe
+function assignGroup() {
+    const groups = ['CF', 'FI'];
+    const randomIndex = Math.floor(Math.random() * groups.length);
+    return groups[randomIndex];
+}
+
+async function id_api_call() {
+    const apiUrl = 'https://controlling.xaidemo.de/api/id'; //Sicherstellen, dass diese URL korrekt ist
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const user = await response.json();
+        return user.id;
+    } catch (error) {
+        console.error('Error in id_api_call:', error);
+        throw error;
+    }
+}
+
+async function btn_click() {
+    let group = assignGroup(); // Benutzer in eine Gruppe einteilen
+    try {
+        let uuid = await id_api_call(); // Hol dir die reine UUID
+        let id = `${group}-${uuid}`; // Kombiniere die UUID mit dem Gruppenvorsatz für andere Zwecke
+
+        // Speichere die UUID und Gruppe in localStorage
+        localStorage.setItem("user_uuid", uuid);
+        localStorage.setItem("user_group", group);
+        
+        send_feedback(uuid, group); // Nur die reine UUID an die API senden
+        window.location.href = "setting.html?id=" + id; // Präfix weiterhin für die URL verwenden
+    } catch (error) {
+        console.error('Error in btn_click:', error);
+    }
+}
+
 async function send_feedback(uuid, group) {
     try {
         const rawResponse = await fetch("https://controlling.xaidemo.de/api/clicks", {
