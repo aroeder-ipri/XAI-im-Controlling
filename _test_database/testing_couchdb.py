9141fc638832ae3d586380ff8d10639c9259c829
Laura Gaddum <gaddula@web.de>
2025-02-25 09:26:20 +0100

    saving the jsons from the database in a csv file

--- _test_database/testing_couchdb.py
+++ _test_database/testing_couchdb.py
@@ -1,4 +1,5 @@
 import couchdb
+import csv
 
 def main():
     # Connect to CouchDB
@@ -9,10 +10,22 @@ def main():
     db = couch['click_events']
 
     # Fetch all documents in the database
+    data_list = []
     for doc_id in db:
         doc = db[doc_id]
         print("Document ID:", doc_id)
         print("Data:", doc)
+
+        data_list.append(doc)
+
+    # saving data list as csv file in /home/ubuntu/XAI-im-Controlling/_test_database
+    keys = data_list[0].keys() if data_list else []
+    filename = "/home/ubuntu/XAI-im-Controlling/_test_database/data.csv"
+    with open(filename, 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data_list)
+
 
 
 if __name__ == "__main__":
