f35a2dd0b98dd2afeba379ec87b5f80c9f2a892b
Laura Gaddum <gaddula@web.de>
2025-02-26 15:12:44 +0100

    new file to get data from database in csv format

--- _test_database/getting_data.py
+++ _test_database/getting_data.py
@@ -1,4 +1,5 @@
 import couchdb
+import pandas as pd
 
 def main():
     # Connect to CouchDB
@@ -8,11 +9,19 @@ def main():
     # Select the database
     db = couch['click_events']
 
+    data_list = []
     # Fetch all documents in the database
     for doc_id in db:
         doc = db[doc_id]
         print("Document ID:", doc_id)
         print("Data:", doc)
+        data_list.append(doc)
+
+    # Convert the list of dictionaries to a DataFrame
+    df = pd.DataFrame(data_list)
+
+    # Saving the dataframe as csv
+    df.to_csv('/home/ubuntu/XAI-im-Controlling/_test_database/click_events.csv', index=False)
 
 
 if __name__ == "__main__":
